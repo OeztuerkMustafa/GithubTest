2a3f8a7f63bd5f236fd399116e65b700c037fd99
OeztuerkMustafa <mustafa88_67@hotmail.de>
2015-10-27 14:22:38 +0100

    Initial commit

---
+++ src/Test/HalloTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+/**
+ * Created by Mustafa Öztürk on 27.10.2015.
+ */
+public class HalloTest {
+
+
+
+    public static void main (String args[]){
+
+        System.out.println("Das ist ein Test!");
+    }
+}
